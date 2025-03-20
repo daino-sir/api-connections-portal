45092e64de6fc9aab3d0d3ddce9fdc5880eb41ad
Daino Otieno <72624000+daino-sir@users.noreply.github.com>
2025-03-20 07:20:47 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+
+export default function App() {
+  return (
+    <>
+    {/* import and add your Components here */}
+    { /* Caution: Do not replace the fragments with a div, will result in a horizontal scroll */ }    
+    </>
+  );
+}
+
