1aecb3d546d8455e17f81d89cb324ff6051d7c09
Brandonflex <brandonflex108@gmail.com>
2025-04-16 13:47:35 +0300

    Navbar
    For this commit, i was mainly focusing on buiilding the navbar. The navbar includes The Home, about, services and contact features. On my next commit, i will be working on configuring each feature to its own designated page.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,17 @@
 import Image from "next/image";
 import PageWrapper from "@/components/ui/wrappers/page-wrapper";
+import { NavbarWrapper } from "@/components/ui/wrappers/navbar-wrapper";
 
 export default function Home() {
   return (
-    <PageWrapper>
-      <Image src="/next.svg" alt="Next.js Logo" height={300} width={400} priority />
-    </PageWrapper>
+    <>
+      <NavbarWrapper>
+        {/* Add content or leave empty if no children are needed */}
+        <div></div>
+      </NavbarWrapper>
+      <PageWrapper>
+        <Image src="/next.svg" alt="Next.js Logo" height={300} width={400} priority />
+      </PageWrapper>
+    </>
   );
 }
