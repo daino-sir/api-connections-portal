3c2ee507a3889822773e59a10aa48511dbe5bb31
Brandonflex <brandonflex108@gmail.com>
2025-04-24 14:48:13 +0300

    Products Page

---
+++ src/app/products/page.js
@@ -0,0 +1,54 @@
+"use client";
+import React, { useState } from "react";
+import Navbar from "@/components/ui/navbar/navbar";
+import "./products.css";
+
+const ProductsPage = () => {
+  const [cart, setCart] = useState([]);
+
+  const products = [
+    { id: 1, name: "Apple", price: 100 },
+    { id: 2, name: "Banana", price: 50 },
+    { id: 3, name: "Onions", price: 80 },
+    { id: 4, name: "Ground Nuts", price: 120 },
+    { id: 5, name: "Skuma wiki", price: 30 },
+    { id: 6, name: "Tomatoes", price: 70 },
+    { id: 7, name: "Cabbages", price: 60 },
+    { id: 8, name: "Ginger", price: 150 },
+    { id: 9, name: "A crate of eggs", price: 350 },
+    { id: 10, name: "Avocados", price: 30 },
+    { id: 11, name: "Spinach", price: 40 },
+    { id: 12, name: "Mangoes", price: 90 },
+  ];
+
+  const addToCart = (productName) => {
+    setCart([...cart, productName]);
+    alert(`${productName} added to cart!`);
+  };
+
+  return (
+    <>
+      <Navbar />
+      <div className="products-container">
+        <h1>Products</h1>
+        <ul className="products-list">
+          {products.map((product) => (
+            <li key={product.id} className="product-item">
+              <span>
+                {product.name} - Ksh {product.price.toFixed(2)}/-
+              </span>
+              <button
+                onClick={() => addToCart(product.name)}
+                className="add-to-cart-button"
+              >
+                Add to Cart
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
+  );
+};
+
+export default ProductsPage;
