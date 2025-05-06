ddbbbe03270d66fe38ac9fa79f696c6fcb61b568
Brandonflex <brandonflex108@gmail.com>
2025-05-06 12:11:24 +0300

    Fixing Function and adding more product categories

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -14,12 +14,33 @@ const ProductsPage: React.FC = () => {
     { id: 6, name: "Tomatoes", price: 70, category: "Vegetables" },
     { id: 7, name: "Cabbages", price: 60, category: "Vegetables" },
     { id: 8, name: "Ginger", price: 150, category: "Vegetables" },
-   
+    { id: 9, name: "A crate of eggs", price: 350, category: "Dairy" },
+    { id: 10, name: "Avocados", price: 30, category: "Fruits" },
+    { id: 11, name: "Spinach", price: 40, category: "Vegetables" },
+    { id: 12, name: "Mangoes", price: 90, category: "Fruits" },
+    { id: 9, name: "A crate of eggs", price: 350, category: "Dairy" },
+    { id: 10, name: "Avocados", price: 30, category: "Fruits" },
+    { id: 11, name: "Spinach", price: 40, category: "Vegetables" },
+    { id: 12, name: "Mangoes", price: 90, category: "Fruits" },
+    { id: 13, name: "Pineapples", price: 120, category: "Fruits" },
+    { id: 14, name: "Yogurt", price: 160, category: "Dairy" },
+    { id: 15, name: "Mala", price: 65, category: "Dairy" },
+    { id: 16, name: "Wow rings", price: 50, category: "Snacks" },
+    { id: 17, name: "TikTac", price: 60, category: "Snacks" },
+    { id: 18, name: "Minute maid", price: 80, category: "Snacks" },
+    { id: 19, name: "Ice Cream", price: 150, category: "Snacks" },
+    { id: 20, name: "Milk", price: 60, category: "Dairy" },
+    { id: 21, name: "Margarine", price: 80, category: "Dairy" },
+    { id: 22, name: "Popcorn", price: 130, category: "Snacks" },
+    { id: 23, name: "Corn Flakes", price: 270, category: "Snacks" },
   ];
 
-  const categories = ["All", "Fruits", "Vegetables",];
+  const categories = ["All", "Fruits", "Vegetables", "Snacks", "Dairy"];
 
-  
+  const filteredProducts =
+    selectedCategory === "All"
+      ? products
+      : products.filter((product) => product.category === selectedCategory);
 
   return (
     <>
